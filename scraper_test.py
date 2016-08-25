3be08f232390beb2901e78ac759456b054584b0a
Mateus Resende <mateus.rsnd@gmail.com>
2016-08-24 23:56:06 -0300

    mudanca no seletor usado

--- scraper_test.py
+++ scraper_test.py
@@ -23,15 +23,16 @@ tree = html.fromstring(product_page.content)
 # Reparem que uso o html+css da página para reconhecer um elemento desejado
 data['name'] = tree.xpath("//b[@itemprop='name']/text()")[0].strip()
 data['price'] = tree.xpath("//i[@class='sale price']/text()")[0].strip()
-data['processor'] = tree.xpath('//dl[@id="ctl00_Conteudo_ctl60_DetalhesProduto_rptGrupos_ctl00_rptCampos_ctl00_dlCategoria"]/dd/text()')[0].strip()
-data['screen_size'] = tree.xpath('//dl[@id="ctl00_Conteudo_ctl60_DetalhesProduto_rptGrupos_ctl00_rptCampos_ctl03_dlCategoria"]/dd/text()')[0].strip()
-data['operational_system'] = tree.xpath('//*[@id="ctl00_Conteudo_ctl60_DetalhesProduto_rptGrupos_ctl00_rptCampos_ctl02_dlCategoria"]/dd/text()')[0].strip()
-data['color'] = tree.xpath('//*[@id="ctl00_Conteudo_ctl60_DetalhesProduto_rptGrupos_ctl00_rptCampos_ctl10_dlCategoria"]/dd/text()')[0].strip()
-data['ram_memory'] = tree.xpath('//*[@id="ctl00_Conteudo_ctl60_DetalhesProduto_rptGrupos_ctl01_rptCampos_ctl01_dlCategoria"]/dd/text()')[0].strip()
-data['battery'] = tree.xpath('//*[@id="ctl00_Conteudo_ctl60_DetalhesProduto_rptGrupos_ctl01_rptCampos_ctl11_dlCategoria"]/dd/text()')[0].strip()
+data['processor'] = tree.xpath('//*[@class="Processador"]/dd/text()')[0].strip()
+data['screen_size'] = tree.xpath('//dl[@class="Tamanho-da-tela even"]/dd/text()')[0].strip()
+data['operational_system'] = tree.xpath('//*[@class="Sistema-operacional"]/dd/text()')[0].strip()
+data['color'] = tree.xpath('//*[@class="Cor"]/dd/text()')[0].strip()
+data['ram_memory'] = tree.xpath('//*[@class="Memoria-RAM even"]/dd/text()')[0].strip()
+data['battery'] = tree.xpath('//*[@class="Bateria even"]/dd/text()')[0].strip()
 
 # Impressão dos dados na tela com identação de quatro espaços e ordenação
 # das chaves do json
 print json.dumps(data, indent=4, sort_keys=True)
+
 
 
